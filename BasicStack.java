3fee9b5682476736efa154fa8f110aac7ac5e713
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-01-06 04:09:10 +0800

    putting items in stack but we wont see LIFO in action

---
+++ BasicStack.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+public class BasicStack {
+    
+    public static void main (String [] args){
+
+        Stack <String> exampleStack = new Stack<>();
+
+        // push items/elements into stack name = exampleStack
+
+        exampleStack.push("2026");
+        exampleStack.push ("year");
+        exampleStack.push("Welcome!");
+
+        System.out.println(exampleStack);
+
+    }
+}
