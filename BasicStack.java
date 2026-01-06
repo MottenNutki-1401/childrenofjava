c0def9961d76f48b4b8a7526bd138c5e1b68fc7b
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-01-07 00:17:45 +0800

    Added comments, we cant see lifo in action yet

--- BasicStack.java
+++ BasicStack.java
@@ -5,13 +5,15 @@ public class BasicStack {
 
         Stack <String> exampleStack = new Stack<>();
 
-        // push items/elements into stack name = exampleStack
+        // push items/elements into stack name = exampleStack 
+        //using push() method -> push method adds element to the top of the stack
 
         exampleStack.push("2026");
         exampleStack.push ("year");
         exampleStack.push("Welcome!");
 
-        System.out.println(exampleStack);
+        System.out.println(exampleStack); //this method only shows storage order from indec 0 to 2
+        // hence we can't see lifo in action 
 
     }
 }
