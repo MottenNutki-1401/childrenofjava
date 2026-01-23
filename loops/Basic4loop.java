26a2a3b9b03cceae81451de606298a12fab8b1ad
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-01-23 19:43:52 +0800

    Loops code samples

---
+++ loops/Basic4loop.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class Basic4loop {
+    public static void main (String[] args){
+        for (int i=1; i<=5; i++) {
+            System.out.println (i);
+        }
+    }
+    
+}
